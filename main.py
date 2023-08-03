5e0718b89419d864aeb6431b640347076828e6df
Johan Fritiofsson <johan.fritiofsson@lime.tech>
2023-08-03 10:42:10 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print(f'Hello {input("Enter your name: ")}!')
+age = int(input("Enter your age: "))
+
+if age > 17:
+    print("You are allowed to take a drivers licence!")
+else:
+    print("You are not allowed to take a drivers licence")
