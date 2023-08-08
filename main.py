28ed6c5a8125cfa9e2f9f08af8ecb35916f88f98
Johan Fritiofsson <88431894+JFritLime@users.noreply.github.com>
2023-08-08 14:36:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 name = input("Enter your name: \n")
 age = int(input("Enter your age: \n"))
 
-if age >= 18:
+if age >= 16:
     print(f"{name}, you are allowed to take a drivers licence!")
 else:
     print(f"{name}, you are not allowed to take a drivers licence")
